c5d27480cc12fa95b49ba46294c7a02a560a8cc9
sotiriosc <chortogiannoss@gmail.com>
2023-04-23 20:55:12 -0400

    created new pages

--- submit-form.php
+++ submit-form.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
   $headers = "From: $email";
   
-  if (mail($to, $subject, $body, $headers)) {
+  if (mail('chortogiannoss@gmail.com', $subject, $body, $headers)) {
     header("Location: contact.html?status=success");
   } else {
     header("Location: contact.html?status=error");
