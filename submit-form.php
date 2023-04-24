93746bba2b6524a87b6bac83dc5c42820ac84ea5
sotiriosc <chortogiannoss@gmail.com>
2023-04-23 20:51:57 -0400

    fixed bug

--- submit-form.php
+++ submit-form.php
@@ -1,24 +1,21 @@
 <?php
-echo "PHP code is being executed!";
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-
-    $to = "chortogiannoss@gmail.com";
-    $subject = "New Message from $name";
-    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-
-    $headers = "From: $email";
-
-    if (mail($to, $subject, $body, $headers)) {
-        $status = "success";
-        $response = "Message sent successfully!";
-    } else {
-        $status = "error";
-        $response = "There was a problem sending your message. Please try again.";
-    }
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $to = 'your-email@example.com'; // Replace with your own email address
+  $subject = "New message from $name";
+  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+  $headers = "From: $email";
+  
+  if (mail($to, $subject, $body, $headers)) {
+    header("Location: contact.html?status=success");
+  } else {
+    header("Location: contact.html?status=error");
+  }
 }
 ?>
 
