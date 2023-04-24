78ee05fac6d4ba5bdf6923131ce2f0a88fb1fc67
sotiriosc <chortogiannoss@gmail.com>
2023-04-23 21:03:51 -0400

    trouble shooting

--- submit-form.php
+++ submit-form.php
@@ -6,12 +6,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST['email'];
   $message = $_POST['message'];
   
-  $to = 'your-email@example.com'; // Replace with your own email address
+  $to = 'chortogiannoss@gmail.com'; // Replace with your own email address
   $subject = "New message from $name";
   $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
   $headers = "From: $email";
   
-  if (mail('chortogiannoss@gmail.com', $subject, $body, $headers)) {
+  if (mail($to, $subject, $body, $headers)) {
     header("Location: contact.html?status=success");
   } else {
     header("Location: contact.html?status=error");
